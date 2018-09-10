76fc3f8455a0094549fd35c92ab348ec2a7b158b
SLS\mahesh.tadepally <mahesh.tadepally@sharecare.com>
2018-09-10 20:14:24 +0530

    updated screen

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,47 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ActionSheetController } from 'ionic-angular'
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  name:any;
+  gender :any;
+  someCondition:any;
+  constructor(public navCtrl: NavController, public actionSheetCtrl: ActionSheetController) {
+    this.gender  = "f";
+    this.someCondition = true;
 
-  constructor(public navCtrl: NavController) {
-
-  }
-
-}
+   }
+    presentActionSheet() {
+      let actionSheet = this.actionSheetCtrl.create({
+        title: 'Modify your album',
+        buttons: [
+          {
+            text: 'Destructive',
+            role: 'destructive',
+            handler: () => {
+              console.log('Destructive clicked');
+            }
+          },
+          {
+            text: 'Archive',
+            handler: () => {
+              console.log('Archive clicked');
+            }
+          },
+          {
+            text: 'Cancel',
+            role: 'cancel',
+            handler: () => {
+              console.log('Cancel clicked');
+            }
+          }
+        ]
+      });
+   
+      actionSheet.present();
+    }
+   }
